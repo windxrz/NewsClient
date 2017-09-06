cd7dab68db25549cbbd03780cb2b9b415a42d114
Xu Renzhe <xrz199721@126.com>
2017-09-06 20:06:11 +0800

    尝试添加fragment

--- app/src/main/java/babydriver/newsclient/MainActivity.java
+++ app/src/main/java/babydriver/newsclient/MainActivity.java
@@ -1,15 +1,22 @@
 package babydriver.newsclient;
 
 import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity
 {
-
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        HomeFragment home_fragment = new HomeFragment();
+        FragmentManager fragment_manager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragment_manager.beginTransaction();
+        transaction.add(R.id.Fragment, home_fragment);
+        transaction.commit();
     }
 }
