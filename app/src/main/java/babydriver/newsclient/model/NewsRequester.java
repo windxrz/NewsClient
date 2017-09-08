8d7116fb3fc831b2bb3dd4b028f9df1519660870
Starcutter <zuohaojia@126.com>
2017-09-08 23:21:19 +0800

    NewsRequest添加获取并缓存图片

--- app/src/main/java/babydriver/newsclient/model/NewsRequester.java
+++ app/src/main/java/babydriver/newsclient/model/NewsRequester.java
@@ -1,9 +1,18 @@
 package babydriver.newsclient.model;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.annotation.NonNull;
+import android.util.Log;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Map;
 
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -19,6 +28,7 @@ public class NewsRequester
     private LatestService latestService;
     private SearchService searchService;
     private DetailService detailService;
+    private PictureService pictureService;
 
     public NewsRequester()
     {
@@ -31,6 +41,7 @@ public class NewsRequester
         latestService = retrofit.create(LatestService.class);
         searchService = retrofit.create(SearchService.class);
         detailService = retrofit.create(DetailService.class);
+        pictureService = retrofit.create(PictureService.class);
     }
 
     public void requestLatest(Map<String, Integer> map, final onRequestListener<NewsBriefList> listener)
@@ -110,6 +121,62 @@ public class NewsRequester
 
             }
         });
+    }
+
+    public void requestPicture(String picUrl, final String cacheDir, final onRequestListener<Bitmap> listener)
+    {
+        Call<ResponseBody> pictureCall = pictureService.downloadPic(picUrl);
+        pictureCall.enqueue(new Callback<ResponseBody>()
+        {
+            @Override
+            public void onResponse(@NonNull Call<ResponseBody> call, @NonNull Response<ResponseBody> response)
+            {
+                if (response.isSuccessful())
+                {
+                    Bitmap bm = savePicToDisk(cacheDir, response.body());
+                    listener.onSuccess(bm);
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<ResponseBody> call, @NonNull Throwable t)
+            {
+
+            }
+        });
+    }
+
+    private Bitmap savePicToDisk(String cacheDir, ResponseBody body)
+    {
+        Bitmap bm = null;
+        InputStream inputStream = null;
+        File file = new File(cacheDir);
+        try
+        {
+            inputStream = body.byteStream();
+            bm = BitmapFactory.decodeStream(inputStream);
+            FileOutputStream outputStream = new FileOutputStream(file);
+            bm.compress(Bitmap.CompressFormat.JPEG,90,outputStream);
+            outputStream.flush();
+            outputStream.close();
+        } catch (FileNotFoundException e)
+        {
+            Log.e("Exception", "filenotfound");
+        } catch (IOException e)
+        {
+            Log.e("Exception", "ioexception when flushing/closing outputStream");
+        } finally
+        {
+            if (inputStream != null)
+                try
+                {
+                    inputStream.close();
+                } catch (IOException e)
+                {
+                    Log.e("Exception", "ioexception when closing inputStream");
+                }
+        }
+        return bm;
     }
 
     public interface onRequestListener<T>
